ed2127ac045f52364615bf362ac530f8ad029931
Michael Staszel <mike@simplerelevance.com>
2013-07-09 16:59:07 -0500

    Update binder.py

--- tweepy/binder.py
+++ tweepy/binder.py
@@ -4,6 +4,7 @@
 
 import httplib
 import urllib
+import urllib2
 import time
 import re
 from StringIO import StringIO
@@ -130,10 +131,11 @@ def bind_api(**config):
             retries_performed = 0
             while retries_performed < self.retry_count + 1:
                 # Open connection
-                if self.api.secure:
-                    conn = httplib.HTTPSConnection(self.host, timeout=self.api.timeout)
-                else:
-                    conn = httplib.HTTPConnection(self.host, timeout=self.api.timeout)
+                if not self.api.proxy:
+                    if self.api.secure:
+                        conn = httplib.HTTPSConnection(self.host)
+                    else:
+                        conn = httplib.HTTPConnection(self.host)
 
                 # Apply authentication
                 if self.api.auth:
@@ -148,8 +150,18 @@ def bind_api(**config):
 
                 # Execute request
                 try:
-                    conn.request(self.method, url, headers=self.headers, body=self.post_data)
-                    resp = conn.getresponse()
+                    if not self.api.proxy:
+                        conn.request(self.method, url, headers=self.headers, body=self.post_data)
+                        resp = conn.getresponse()
+                    else:
+                        opener = urllib2.build_opener(
+                            urllib2.HTTPHandler(),
+                            urllib2.HTTPSHandler(),
+                            urllib2.ProxyHandler({'https': 'http://'+self.api.proxy, 'http': 'http://'+self.api.proxy}))
+                        murl = self.scheme + self.host + url
+                        resp = opener.open(urllib2.Request(murl, self.post_data, headers=self.headers));
+                        resp.status = resp.getcode()
+                        
                 except Exception, e:
                     raise TweepError('Failed to send request: %s' % e)
 
@@ -182,7 +194,8 @@ def bind_api(**config):
                     raise TweepError('Failed to decompress data: %s' % e)
             result = self.api.parser.parse(self, body)
 
-            conn.close()
+            if not self.api.proxy:
+                conn.close()
 
             # Store result into cache if one is available.
             if self.use_cache and self.api.cache and self.method == 'GET' and result:
